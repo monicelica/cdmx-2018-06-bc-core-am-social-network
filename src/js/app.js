d2f203e6fc03a85629f26e09aeab0267af50c6e0
Mónica Pavía <monicelica_pavi@hotmail.com>
2018-07-30 13:35:45 -0500

    verificando pull

--- src/js/app.js
+++ src/js/app.js
@@ -10,54 +10,54 @@ const registrar= () => {
         // Handle Errors here.
         let errorCode = error.code;
         let errorMessage = error.message;
-        console.log(errorCode);
+        alert(errorCode);
         console.log(errorMessage);
         // ...
       });
 }
 
 const loginGoogle= () => {
-    var provider = new firebase.auth.GoogleAuthProvider();
+    let provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+        let token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user;
+        let user = result.user;
         console.log(result)
         // ...
       }).catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
+        let email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        let credential = error.credential;
         // ...
-    
+
       });
 }
 
 const loginFace= () => {
-    var provider = new firebase.auth.FacebookAuthProvider();
+    let provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         provider.addScope('public_profile');
         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+        let token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user;
+        let user = result.user;
         console.log(result)
         // ...
       }).catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
+        let email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        let credential = error.credential;
         // ...
-    
+
       });
 }
 
@@ -75,7 +75,7 @@ const ingreso = () => {
         let errorCode = error.code;
         let errorMessage = error.message;
         console.log(errorCode);
-        console.log(errorMessage);
+        alert(errorMessage);
         // ...
       });
 }
@@ -84,37 +84,39 @@ const observador = () => {
     //cuando un usuario ya esta registrado
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          console.log("existe usuario registrado");
-          // User is signed in. 
-          aparece();
-          var displayName = user.displayName;
-          var email = user.email;
+          // console.log("existe usuario registrado");
+          // User is signed in.
+          // aparece();
+          // let displayName = user.displayName;
+          // let email = user.email;
           //cuando el usuario ya confirmo correo
-          console.log(user.emailVerified);
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
+          // console.log(user.emailVerified);
+          // let emailVerified = user.emailVerified;
+          // let photoURL = user.photoURL;
+          // let isAnonymous = user.isAnonymous;
+          // let uid = user.uid;
+          // let providerData = user.providerData;
           // si no existe un usuario
+
+          // guarda session en localStorage
+          window.localStorage.setItem('session', JSON.stringify({
+            token: user.G,
+            email: user.email,
+            photoURL: user.photoURL,
+            phoneNumber: user.phoneNumber
+          }));
+
+          // redirecciona al timeline
+          return location.href = './views/timeline.html';
         } else {
           // User is signed out.
-          console.log("no existe usuario registrado");
+          console.log("Registra tu usuario");
           // ...
         }
       });
 }
+
 observador();
-
-
-//contenido que slo se mostrara al estar registrado
-const aparece = () =>{
-    let contenido = document.getElementById("contenido");
-        contenido.innerHTML = `
-        <h4>BIENVENIDO</h4>
-        <button onclick = "cerrar()">cerrar sesión</button>
-        `; 
-};
 
 const cerrar = () => {
     firebase.auth().signOut()
@@ -122,22 +124,22 @@ const cerrar = () => {
         console.log("Saliendo...")
     })
     .catch(function(error){
-        console.log(eror)
+        console.log(error);
     })
 };
 //verificar el correo electronico con el que se esta registrando
 const verificar = () =>{
     //enviara correo para verificar
-    var user = firebase.auth().currentUser;
+    let user = firebase.auth().currentUser;
     //usuario a quien se le mandara correo
     user.sendEmailVerification()
     .then(function() {
     //si funciona se le mandara correo
     // Email sent.
-    console.log("Enviando correo..");
+    alert("Enviando correo..");
     }).catch(function(error) {
     // An error happened.
-    console.log("error");
+    alert("error");
     });
 
 }
