8db261b6ffd75dce49963a61ba9af7b8273a70e7
Mónica Pavía <monicelica_pavi@hotmail.com>
2018-07-30 20:40:48 -0500

    Se realizo maquetacion del home y especialistas, se hizo todo responsive, se agrego funcionalidad

--- src/js/app-coment-movil.js
+++ src/js/app-coment-movil.js
@@ -7,18 +7,18 @@ firebase.initializeApp({
   //  DB Initialize Cloud Firestore through Firebase
   let db = firebase.firestore();
   
-const guardar = () =>{
+const guardar1 = () =>{
     //obtener valores
-    let comentario = document.getElementById('comentario').value;
+    let comentario1 = document.getElementById('comentario1').value;
     db.collection('users').add({
-        first: comentario
+        first: comentario1
         
     })
     //se agrego correctamente lo anterior aparecera el mensaje del docRef.id
     .then(docRef=> {
         console.log('Document written with ID: ', docRef.id);
         //limpiar campos al enviar ya un comentario
-        let comentario=document.getElementById('comentario').value = '';
+        let comentario1=document.getElementById('comentario1').value = '';
     })
     .catch(error=> {
         console.error('Error adding document: ', error);
@@ -27,33 +27,33 @@ const guardar = () =>{
 }
 
 //para poder leer y pintar los datos 
-let paint = document.getElementById('getcomentario').value;
+let paint = document.getElementById('getcomentario1').value;
 //LLAMA DB (INICIACION DE FIRESTORE) EN NUESTRO CASO LA COLECCIÓN ES USUARIO
 //se susutituye .get para que este mostrando los datos en tiempo real
 db.collection('users').onSnapshot((querySnapshot) => {
-    getcomentario.innerHTML='';
+    getcomentario1.innerHTML='';
     //FOR EACHE SE REPIETIRA EN CADA DOCUMENTO DE USER
     querySnapshot.forEach((doc) => {
         //SE PINTARA EL ID Y LO QUE CONTIENE
         console.log(`${doc.id} => ${doc.data().first}`);
         
-        getcomentario.innerHTML += `
+        getcomentario1.innerHTML += `
         <div class="comentario-publicado">
             <div class="comentario">${doc.data().first}</div>
             <!--BOTONES-->
             <!--Por cada boton que se crea contiene los datos-->
             <div class="all-botones">
                 <button type="button" class="btn boton-comentario" onclick="editar('${doc.id}', '${doc.data().first}')">
-                    <img src="../images/editar2.png" class="img-comentario" alt="editar">Editar
+                    <img src="../images/editar1.png" class="img-comentario" alt="editar">Editar
                 </button>
                 <button type="button" class="btn boton-comentario" onclick="eliminar('${doc.id}')">
-                    <img src="../images/cubo-de-la-basura.png" class="img-comentario" alt="editar">Borrar
+                    <img src="../images/cubo-de-la-basura1.png" class="img-comentario" alt="editar">Borrar
                 </button>
                 <button type="button" class="btn boton-comentario">
-                    <img src="../images/corazon-en-boceto.png" class="img-comentario" alt="editar">Like
+                    <img src="../images/corazon-en-boceto1.png" class="img-comentario" alt="editar">Like
                 </button>
                 <button type="button" class="btn boton-comentario">
-                    <img src="../images/corazon.png" class="img-comentario" alt="editar">Dislike
+                    <img src="../images/corazon1.png" class="img-comentario" alt="editar">Dislike
                 </button>
             </div>
         </div>
@@ -76,26 +76,26 @@ const eliminar = (id) => {
 
 //editar datos
 
- const editar = (id,comentario) =>{
+ const editar1 = (id,comentario1) =>{
 
-    document.getElementById('comentario').value = comentario;
+    document.getElementById('comentario1').value = comentario1;
     //el boton con este id"guardar" se modificara su texto html a editar
-    let editar = document.getElementById('editar');
+    let editar1 = document.getElementById('editar1');
     // editar.innerHTML = 'Editar';
    
     //Al presionar el boton q dice editar c correra la sig funcion
-       editar.onclick= function() { 
+       editar1.onclick= function() { 
         let washingtonRef = db.collection('users').doc(id);
         // Set the "capital" field of the city 'DC'
-    let comentario = document.getElementById('comentario').value;
+    let comentario1 = document.getElementById('comentario1').value;
     return washingtonRef.update({
-        first: comentario
+        first: comentario1
        
     })
     .then(fuction=> {
         console.log('Document successfully updated!');
         // boton.innerHTML = 'Editar';
-        document.getElementById('comentario').value='';
+        document.getElementById('comentario1').value='';
     })
     .catch(error=> {
         // The document probably doesn't exist.
